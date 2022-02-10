33a339a269fc47fba87174f131e3f68f7a83e03a
yukinissie <bigtree2490@outlook.jp>
2022-02-10 11:18:18 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 西勇樹 on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
