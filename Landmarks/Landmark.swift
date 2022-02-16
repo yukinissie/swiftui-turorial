d44210b5284e302083e485564fd7ce44f720cac7
yukinissie <bigtree2490@outlook.jp>
2022-02-16 20:58:55 +0900

    2-1終了

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by 西勇樹 on 2022/02/16.
+//
+
+import Foundation
---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by 西勇樹 on 2022/02/16.
+//
+
+import Foundation
