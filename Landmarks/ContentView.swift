c69f96e92c02efbc13a3f47d732907a1dd28fe87
yukinissie <bigtree2490@outlook.jp>
2022-02-16 19:45:15 +0900

    第３節終了

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-		Text("Turtle Rock")
-			.font(.title)
+		VStack(alignment: .leading) {
+			Text("Turtle Rock")
+				.font(.title)
+			HStack {
+				Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+					.font(.subheadline)
+				Spacer()
+				Text("California")
+					.font(.subheadline)
+			}
+		}
+		.padding()
 
     }
 }
